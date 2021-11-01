bb5da80913109048053c82d5945b1c6e1e06e055
Syed M. Sawaid <34231494+SyedMSawaid@users.noreply.github.com>
2021-11-01 13:57:38 +0500

    Module 5

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -18,5 +18,42 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var results = _context.CelestialObjects.ToList();
+            foreach (var result in results)
+            {
+                result.Satellites = _context.CelestialObjects.Where(s => s.OrbitedObjectId == result.Id).ToList();
+            }
+
+            return Ok(results);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObjects = _context.CelestialObjects.Find(id);
+            if (celestialObjects == null)
+            {
+                return NotFound();
+            }
+
+            celestialObjects.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == id).ToList();
+            return Ok(celestialObjects);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var result = _context.CelestialObjects.Find(name);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            result.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == result.Id).ToList();
+            return Ok(result);
+        }
     }
 }
